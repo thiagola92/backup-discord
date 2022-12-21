7262439a0243620c672b6904271d2615b3bc4bfc
thiagola92 <thiagola92@gmail.com>
2022-12-21 05:07:58 -0300

    Update requirements and add more gifs

--- run.py
+++ run.py
@@ -1,11 +1,14 @@
 import os
-import requests
 from pathlib import Path
-from discord import Client
 from urllib.parse import urlparse
 
+import requests
+from discord import Client, Intents
 
-client = Client()
+intents = Intents.default()
+intents.message_content = True
+
+client = Client(intents=intents)
 
 
 @client.event
@@ -17,7 +20,7 @@ async def on_ready():
 async def on_message(message):
     if not message.content.startswith("$backup") or message.author.bot:
         return
-    
+
     print(f"CHANNEL >> {message.channel}")
 
     async for m in message.channel.history(limit=None):
@@ -26,11 +29,11 @@ async def on_message(message):
 
         if m.author == message.author:
             download_content(m.content)
-    
+
     print("END")
 
 
-def download_content(url, suffix='.gif'):
+def download_content(url, suffix=".gif"):
     if not is_url_valid(url):
         return
 
@@ -39,14 +42,14 @@ def download_content(url, suffix='.gif'):
 
     if not Path(path).suffix:
         return download_content(url + suffix)
-    
+
     if Path(f"backup/{name}").exists():
         return
 
     print(url)
-    
+
     response = requests.get(url)
-    with open(f"backup/{name}", 'wb') as file:
+    with open(f"backup/{name}", "wb") as file:
         file.write(response.content)
 
 
